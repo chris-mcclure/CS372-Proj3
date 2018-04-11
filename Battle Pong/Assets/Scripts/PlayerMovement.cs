dbb660855bffe499739a6e0fe19bb20c034a0a39
Camology <wirelessatari@gmail.com>
2018-04-11 13:25:06 -0800

    created first scene, players moving very slow however

---
+++ Battle Pong/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 50000f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 directionVector = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, 0);
+		GetComponent<Rigidbody> ().MovePosition (transform.position + directionVector * Time.deltaTime);
+	}
+}
