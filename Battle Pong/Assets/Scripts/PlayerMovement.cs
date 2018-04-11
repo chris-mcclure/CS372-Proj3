0e4adb8dce06e62d34b2e3f63ec8ad3f2c3f148b
Camology <wirelessatari@gmail.com>
2018-04-11 14:58:10 -0800

    bounding works properly, movement still slow. Likely tied to my scaling will need to shrink everything

--- Battle Pong/Assets/Scripts/PlayerMovement.cs
+++ Battle Pong/Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	public float speed = 50000f;
+	public float speed = 500f;
+	public GameObject boundingLine;
+
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 directionVector = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, 0);
-		GetComponent<Rigidbody> ().MovePosition (transform.position + directionVector * Time.deltaTime);
+		if (this.GetComponent<Renderer>().bounds.Intersects (boundingLine.GetComponent<Renderer>().bounds)) {
+			Vector3 directionVector = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, 0);
+			GetComponent<Rigidbody> ().MovePosition (transform.position + directionVector * Time.deltaTime);
+		}
 	}
 }
