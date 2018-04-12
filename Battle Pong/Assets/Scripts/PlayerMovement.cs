4677b7c3544895ee76ac7a3c4d56803b31e171dc
Cameron K. Titus <wirelessatari@gmail.com>
2018-04-11 22:44:26 -0800

    fixed camera, fixed bounding sort of

--- Battle Pong/Assets/Scripts/PlayerMovement.cs
+++ Battle Pong/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 	public float speed = 5f;
 	public GameObject boundingLine;
+	private Rigidbody rb;
 
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (this.GetComponent<Renderer>().bounds.Intersects (boundingLine.GetComponent<Renderer>().bounds)) {
 			Vector3 directionVector = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, 0);
-			GetComponent<Rigidbody> ().MovePosition (transform.position + directionVector * Time.deltaTime * speed);
+			rb.MovePosition (rb.position + directionVector * Time.deltaTime * speed);
 		}
+
+		//rb.position = new Vector3 (Mathf.Clamp (rb.position.x, xMin, xMax),0,0);
 	}
 }
