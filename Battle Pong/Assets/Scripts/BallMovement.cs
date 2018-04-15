166a8d5c0bcc060050881cd948f7821f057bd511
ccfortenberry <cmnky@hotmail.com>
2018-04-14 16:28:32 -0800

    Fix ball movement and add paddles with collision

--- Battle Pong/Assets/Scripts/BallMovement.cs
+++ Battle Pong/Assets/Scripts/BallMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour {
 	private Rigidbody _rb;
-	public float _speed;
+	public float _speed = 10f;
 	private float _moveIntialX;
 	private float _moveIntialZ;
 
@@ -17,18 +17,6 @@ public class BallMovement : MonoBehaviour {
 		_moveIntialZ = Random.Range (-1.0f, 1.0f);
 
 		_movement = new Vector3(_moveIntialX, 0, _moveIntialZ);
-	}
-
-	// Update is called once per frame
-	void Update () {
-		_rb.MovePosition (_rb.position + _movement * Time.deltaTime * _speed);
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		if(other.gameObject.CompareTag("Walls"))
-		{
-			_movement = -1 * _movement;
-		}
+		_rb.velocity = _movement * _speed;
 	}
 }
