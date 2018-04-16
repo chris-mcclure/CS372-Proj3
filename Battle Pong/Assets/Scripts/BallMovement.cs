cf700ecf8f5ec347a1e7f1cc8b3eddced666a1f7
ccfortenberry <ccfortenberry@alaska.edu>
2018-04-15 18:42:28 -0800

    fix when last hit is null

--- Battle Pong/Assets/Scripts/BallMovement.cs
+++ Battle Pong/Assets/Scripts/BallMovement.cs
@@ -41,13 +41,15 @@ public class BallMovement : MonoBehaviour {
 		GameObject P2 = GameObject.Find ("P2");
 		PlayerMovement P2Script = (PlayerMovement) P2.GetComponent(typeof(PlayerMovement));
 
-		if (lastHitBy.name == "P1") {
-			P1Script.setScore (P1Script.getScore ()+1);
-			P2Script.setScore (P2Script.getScore ()-1);
-		}
-		if (lastHitBy.name == "P2") {
-			P2Script.setScore (P2Script.getScore ()+1);
-			P1Script.setScore (P1Script.getScore ()-1);
+		if (lastHitBy != null) {
+			if (lastHitBy.name == "P1") {
+				P1Script.setScore (P1Script.getScore () + 1);
+				P2Script.setScore (P2Script.getScore () - 1);
+			}
+			if (lastHitBy.name == "P2") {
+				P2Script.setScore (P2Script.getScore () + 1);
+				P1Script.setScore (P1Script.getScore () - 1);
+			}
 		}
 	}
 
