8bc6ab98b3e215a9c477cbdaa96a182c05ac909f
William Horn <wbhorn@alaska.edu>
2018-04-15 21:46:04 -0800

    Noramlize init velocity;

--- Battle Pong/Assets/Scripts/BallMovement.cs
+++ Battle Pong/Assets/Scripts/BallMovement.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour {
-	private Rigidbody _rb;
-	public float _speed = 10f;
-	private float _moveIntialX;
-	private float _moveIntialZ;
+	private Rigidbody rb;
+	public float speed = 10f;
 
-	private Vector3 _movement;
+	private Vector3 movement;
 
 	// Use this for initialization
 	void Start () {
-		_rb = GetComponent<Rigidbody> ();
-		_moveIntialX = Random.Range (-1.0f, 1.0f);
-		_moveIntialZ = Random.Range (-1.0f, 1.0f);
+		rb = GetComponent<Rigidbody> ();
+		float moveIntialX = Random.Range (-1.0f, 1.0f);
+		float moveIntialZ = Random.Range (-1.0f, 1.0f);
 
-		_movement = new Vector3(_moveIntialX, 0, _moveIntialZ);
-		_rb.velocity = _movement * _speed;
+		movement = new Vector3(moveIntialX, 0, moveIntialZ);
+		rb.velocity = movement.normalized * speed;
 	}
 }
