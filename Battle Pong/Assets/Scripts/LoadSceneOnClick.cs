f4ce9c110ae1ff5832c7722c6185d0d845ea1a1b
FromAlaska <Jim_samson1335@yahoo.com>
2018-04-21 15:22:23 -0800

    Added Networking Scene

--- Battle Pong/Assets/Scripts/LoadSceneOnClick.cs
+++ Battle Pong/Assets/Scripts/LoadSceneOnClick.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
 
-	public void LoadOnClick(int sceneIndex)
-	{
-		SceneManager.LoadScene (sceneIndex);
+	public void LoadOnClick(int sceneIndex) {
+
+		SceneManager.LoadScene ("NetworkingScene");
+		PlayerPrefs.SetInt ("scene", sceneIndex);
+	}
+
+	public void ifPlayerChoosesPlay() {
+		SceneManager.LoadScene (PlayerPrefs.GetInt("scene"));
 	}
 }
