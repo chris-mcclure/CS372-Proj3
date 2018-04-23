370ea8722e4d21c94facc71dd66f2e4a6dcfc6e0
hadimarchi <hal.dimarchi55@gmail.com>
2018-04-23 10:03:28 -0800

    pause in two player mode

---
+++ Battle Pong/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIHandler : MonoBehaviour {
+public GameObject panel;
+void Start()
+{
+        panel = GameObject.Find("Pause_Panel");
+        panel.SetActive(false);
+}
+void Update(){
+        ScanForKeyStroke();
+}
+
+void ScanForKeyStroke(){
+        if (Input.GetKeyDown("escape")) TogglePauseMenu();
+}
+void TogglePauseMenu(){
+        panel.SetActive(!panel.activeSelf);
+        Time.timeScale = !panel.activeSelf ? 1.0f : 0f;
+}
+}
